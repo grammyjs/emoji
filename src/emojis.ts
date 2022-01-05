67ec716b5faa3b0b88a4d11e522984ffb4046ee0
AndreoliBR <andremarcia74@hotmail.com>
2022-01-04 22:28:01 -0400

    minor changes

--- src/emojis.ts
+++ src/emojis.ts
@@ -1,16 +1,18 @@
-import emojis from "./emojis.json" assert { type: "json" };
+import emojis from "./emojis/emojis.json" assert { type: "json" };
 
 interface Emoji {
     name: string;
     emoji: string;
 }
 
+// TODO: Needs fix
 type EmojiList = typeof emojis;
 type EmojiName = keyof EmojiList;
 
+// TODO: Needs fix
 function getEmoji(name: EmojiName): Emoji | undefined {
-    const emoji = emojis[name];
-    return emoji ? { name, emoji } : undefined;
+    const emoji = emojis.find(e => e.description === name);
+    return emoji ? { name: emoji.description, emoji: emoji.emoji } : undefined;
 }
 
 export default getEmoji;
