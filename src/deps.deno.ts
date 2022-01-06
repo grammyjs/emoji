7af440e851c62d9194bf799c6e3439382fb3d200
KnorpelSenf <shtrog@gmail.com>
2022-01-06 23:33:23 +0100

    Fix deps and backporting

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,11 +1,2 @@
-/**
- * Export your deno-specific dependencies from here. Example:
- *
- * export * from "https://lib.deno.dev/x/grammy@1.x/mod.ts";
- * export * from "https://cdn.skypack.dev/@grammyjs/types@v2?dts";
- *
- *  That is if you use deno2node
- */
-
 export * from "https://lib.deno.dev/x/grammy@v1/mod.ts";
 export type { Message } from "https://cdn.skypack.dev/@grammyjs/types@v2?dts";
