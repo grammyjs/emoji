630389421c5e098e03edfa001dc40e1260e892a5
KnorpelSenf <shtrog@gmail.com>
2022-01-06 23:22:25 +0100

    Improve grammY peer dep support on Deno

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -7,5 +7,5 @@
  *  That is if you use deno2node
  */
 
-export * from "https://deno.land/x/grammy@v1/mod.ts";
+export * from "https://lib.deno.dev/x/grammy@v1/mod.ts";
 export type { Message } from "https://cdn.skypack.dev/@grammyjs/types@v2?dts";
