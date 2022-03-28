58b4c145c75b8353f1b22aad82014667fec28dfd
KnorpelSenf <shtrog@gmail.com>
2022-03-29 00:52:41 +0200

    Implement changes

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,8 @@
-import { Context, Message, Middleware } from "./deps.deno.ts";
+import { type Context, type Middleware } from "./deps.deno.ts";
 import getEmoji from "./emoji.ts";
-import type { EmojiName } from "./emoji.ts";
+import { type EmojiName } from "./emoji.ts";
 
-export type EmojiFlavor<C extends Context> = Flavor & C;
-interface Flavor {
+export type EmojiFlavor<C extends Context> = C & {
     /**
      * You can use this method to parse a string with emojis.
      *
@@ -30,8 +29,8 @@ interface Flavor {
     replyWithEmoji: (
         string: TemplateStringsArray,
         ...emojis: EmojiName[]
-    ) => Promise<Message.TextMessage>;
-}
+    ) => ReturnType<C["reply"]>;
+};
 
 function withEmoji(string: TemplateStringsArray, ...emojis: EmojiName[]) {
     return string.reduce((acc, str, index) => {
@@ -40,15 +39,15 @@ function withEmoji(string: TemplateStringsArray, ...emojis: EmojiName[]) {
     }, "");
 }
 
-export function emojiParser<C extends Context & Flavor>(): Middleware<C> {
-    return async (ctx, next) => {
-        ctx.emoji = withEmoji;
-        ctx.replyWithEmoji = (
-            text: TemplateStringsArray,
-            ...emojis: EmojiName[]
-        ): ReturnType<typeof ctx.reply> => {
-            return ctx.reply(withEmoji(text, ...emojis));
-        };
+export function emojiParser<C extends Context>(): Middleware<EmojiFlavor<C>> {
+    return async (ctx: EmojiFlavor<C>, next) => {
+        Object.assign(ctx, {
+            emoji: withEmoji,
+            replyWithEmoji: (
+                text: TemplateStringsArray,
+                ...emojis: EmojiName[]
+            ) => ctx.reply(withEmoji(text, ...emojis)),
+        });
         await next();
     };
 }
