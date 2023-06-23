6e2296ea64e9b93996aac1ec9107cfffdc83e64a
Nazar Antoniuk <75927667+Nazar-Ant@users.noreply.github.com>
2023-06-23 12:10:17 +0300

    Refactoring (#19)
    * refactoring
    
    * removed the charset parameter from tsconfig.json
    
    * optimizations
    
    * Revert "optimizations"
    
    This reverts commit bfe3fb303c3cab4d5e6b84f34ef21a476b37b4f7.
    
    * smaller optimizations while maintaining the code style

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,8 @@
 import { Context, NextFunction } from "./deps.deno.ts";
-import getEmoji from "./emoji.ts";
-import type { EmojiName } from "./emoji.ts";
+import emojis from "./emojidata.ts";
+
+type EmojiList = typeof emojis;
+type EmojiName = keyof EmojiList | (string & Record<never, never>);
 
 export type EmojiFlavor<C extends Context = Context> = C & {
     /**
@@ -32,14 +34,13 @@ export type EmojiFlavor<C extends Context = Context> = C & {
     ) => ReturnType<C["reply"]>;
 };
 
-function withEmoji(string: TemplateStringsArray, ...emojis: EmojiName[]) {
-    return string.reduce((acc, str, index) => {
-        // The number of elements in `string` is 1 more than in `emojis`, therefore
-        // we will be looking for an `undefined` emoji, which does not make sense,
-        // so we exit the loop early.
-        if (index === string.length - 1) return acc + str;
-        const emoji = getEmoji(emojis[index]);
-        return acc + str + (typeof emoji === "string" ? emoji : emoji.emoji);
+export function emoji(name: EmojiName) {
+    return emojis[name as keyof EmojiList] ?? name;
+}
+
+function withEmoji(text: TemplateStringsArray, ...emojis: EmojiName[]) {
+    return text.reduce((acc, str, idx) => {
+        return acc + str + emoji(emojis[idx] ?? "");
     }, "");
 }
 
@@ -52,10 +53,4 @@ export function emojiParser<C extends EmojiFlavor>() {
         ) => ctx.reply(withEmoji(text, ...emojis));
         await next();
     };
-}
-
-export function emoji(name: EmojiName): string {
-    const emoji = getEmoji(name);
-    // `emoji` will be `name` if an emoji with that name doesn't exist.
-    return typeof emoji === "string" ? emoji : emoji.emoji;
 }
